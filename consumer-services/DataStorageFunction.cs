7f3e4617a3604754430e90949c9c03517278bed3
Liam Bonnage <lbonnage@gmail.com>
2020-01-24 22:53:38 -0600

    Added connection to PostgreSQL database Currently no configuration obtained from database

--- consumer-services/DataStorageFunction.cs
+++ consumer-services/DataStorageFunction.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using Npgsql;
 using System.Web.Http;
 
 namespace data_storage
@@ -17,9 +18,13 @@ namespace data_storage
     public static class DataStorageFunction
     {
 
+        // Static MongoDB handles for maintaining a persistent connection across service calls
         static MongoClient mongoClient = null;
         static IMongoDatabase mongoDataStorageDatabase = null;
         static IMongoCollection<BsonDocument> mongoObjectCollection = null;
+
+        // Static PostgreSQL handles for maintaining a persistent connection across service calls
+        static NpgsqlConnection sqlConnection = null;
 
         [FunctionName("DataStorage")]
         public static async Task<IActionResult> Run(
@@ -42,9 +47,10 @@ namespace data_storage
                 return new BadRequestObjectResult("Error parsing data: " + e.Message);
             }
 
-            // In order to preserve the MongoDB client connection across Function calls, perform this check and only connect if necessary
+            // In order to preserve the MongoDB client connection across Service calls, perform this check and only connect if necessary
             if (mongoObjectCollection is null)
             {
+                log.LogInformation("Connecting to MongoDB Database...");
                 try
                 {
                     mongoClient = new MongoClient(System.Environment.GetEnvironmentVariable("MongoDBAtlasConnectionString"));
@@ -58,8 +64,26 @@ namespace data_storage
                 }
             }
 
+            // In order to preserve the PostgreSQL connection across Service calls, perform this check and only connect if necessary
+            if (sqlConnection is null)
+            {
+                log.LogInformation("Connecting to PostgreSQL Database...");
+                try
+                {
+                    sqlConnection = new NpgsqlConnection(System.Environment.GetEnvironmentVariable("PostgreSQLConnectionString"));
+                    sqlConnection.Open();   // TODO should this be OpenAsync()? check with Krishna
+                    log.LogInformation("Connected to PostgreSQL Database");
+                } catch (Exception e)
+                {
+                    log.LogError("Error connecting to PostgreSQL database: " + e.Message);
+                    return new InternalServerErrorResult();
+                }
+            }
+
+            // TODO Retrieve the correct configuration from the SQL database and perform the verification steps here
+
             // Insert the received object into the MongoDB object collection
-            log.LogInformation("Inserting document");
+            log.LogInformation("Inserting document into MongoDB");
             try
             {
                 mongoObjectCollection.InsertOne(document);
