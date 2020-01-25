e1b36ab75a65fc1cea818048501cb703bfd33679
Liam Bonnage <lbonnage@gmail.com>
2020-01-25 00:02:22 -0600

    Configurations can be added to MongoDB Adjusted logging values in both Configuration and DataStorage

--- configuration/ConfigurationFunction.cs
+++ configuration/ConfigurationFunction.cs
@@ -7,27 +7,73 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Web.Http;
 
 namespace configuration
 {
     public static class ConfigurationFunction
     {
+
+        // Static MongoDB handles for maintaining a persistent connection across service calls
+        static MongoClient mongoClient = null;
+        static IMongoDatabase mongoDataStorageDatabase = null;
+        static IMongoCollection<BsonDocument> mongoObjectCollection = null;
+
         [FunctionName("Configuration")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Configuration function received a request.");
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // Parse the message body
+            BsonDocument document;
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                object data = JsonConvert.DeserializeObject(requestBody);
+                document = BsonDocument.Parse(data.ToString());    // TODO is this the correct way to deserialize and read a BSON object?
+            }
+            catch (Exception e)
+            {
+                log.LogError("Error parsing data: " + e.Message);
+                return new BadRequestObjectResult("Error parsing data: " + e.Message);
+            }
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            // In order to preserve the MongoDB client connection across Service calls, perform this check and only connect if necessary
+            if (mongoObjectCollection is null)
+            {
+                log.LogInformation("Connecting to MongoDB Database...");
+                try
+                {
+                    mongoClient = new MongoClient(System.Environment.GetEnvironmentVariable("MongoDBAtlasConnectionString"));
+                    mongoDataStorageDatabase = mongoClient.GetDatabase(System.Environment.GetEnvironmentVariable("DeploymentEnvironment"));
+                    mongoObjectCollection = mongoDataStorageDatabase.GetCollection<BsonDocument>("configurations");
+                    log.LogInformation("Connected to MongoDB Database");
+                }
+                catch (Exception e)
+                {
+                    log.LogError("Error connecting to MongoDB database: " + e.Message);
+                    return new InternalServerErrorResult();
+                }
+            }
+
+            // Insert the received configuration into the MongoDB configuration collection
+            log.LogInformation("Inserting configuration into MongoDB");
+            try
+            {
+                mongoObjectCollection.InsertOne(document);
+            }
+            catch (Exception e)
+            {
+                log.LogError("Failed inserting document into MongoDB database: " + e.Message);
+                return new InternalServerErrorResult();
+            }
+
+            return new OkObjectResult("Succeeded in inserting configuration.");
         }
     }
 }
