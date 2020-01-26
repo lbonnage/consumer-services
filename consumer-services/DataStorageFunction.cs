f1f4d1971cf732deb017323ccdc750b54cf6643c
Liam Bonnage <lbonnage@gmail.com>
2020-01-26 15:53:11 -0600

    AnalysisDocument class is now created within ConfigurationFunction and placed into analysis databasecollection DataStorage performs pre-processing on objects DataAnalysisFunction has infrastructure in place to perform post-processing but as of now only returns an unchanged AnalysisDocument

--- consumer-services/DataStorageFunction.cs
+++ consumer-services/DataStorageFunction.cs
@@ -23,9 +23,10 @@ namespace data_storage
         static MongoClient mongoClient = null;
         static IMongoDatabase mongoConsumerDatabase = null;
         static IMongoCollection<BsonDocument> mongoConfigurationCollection = null;
+        static IMongoCollection<AnalysisDocument> mongoAnalysisCollection = null;
         static Dictionary<string, IMongoCollection<BsonDocument>> mongoObjectCollections = new Dictionary<string, IMongoCollection<BsonDocument>>();
 
-        public static readonly Dictionary<string, string> aliases = new Dictionary<string, string>()
+        static readonly Dictionary<string, string> aliases = new Dictionary<string, string>()
         {
             { "String", "string" },
             { "Int32", "int" },
@@ -85,6 +86,7 @@ namespace data_storage
                     mongoClient = new MongoClient(System.Environment.GetEnvironmentVariable("MongoDBAtlasConnectionString"));
                     mongoConsumerDatabase = mongoClient.GetDatabase(System.Environment.GetEnvironmentVariable("DeploymentEnvironment"));
                     mongoConfigurationCollection = mongoConsumerDatabase.GetCollection<BsonDocument>("configurations");
+                    mongoAnalysisCollection = mongoConsumerDatabase.GetCollection<AnalysisDocument>("analysis");
                     mongoObjectCollections[guid] = mongoConsumerDatabase.GetCollection<BsonDocument>(guid);
                     log.LogInformation("Connected to MongoDB Database");
                 }
@@ -95,12 +97,12 @@ namespace data_storage
                 }
             }
 
-            // Retrieve the correct configuration from the MongoDB database and perform the verification steps here
-            log.LogInformation("Attempting to retrieve configuration for data");
+            // Retrieve the correct configuration document from the MongoDB database
+            log.LogInformation("Attempting to retrieve configuration for ID: " + guid);
             BsonDocument config;
             try
             {
-                var filter = Builders<BsonDocument>.Filter.Eq("_id", guid);
+                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", guid);
                 config = mongoConfigurationCollection.Find<BsonDocument>(filter).First<BsonDocument>();
             }
             catch (Exception e)
@@ -111,8 +113,34 @@ namespace data_storage
 
             log.LogInformation("Retrieved configuration: " + config.GetValue("field_attributes").ToString());
 
+            // Retrieve the correct analysis document from the MongoDB database.  If one doesn't exist for this ID, create one.
+            log.LogInformation("Attempting to retrieve analysis for ID: " + guid);
+            AnalysisDocument analysis;
+            try
+            {
+                FilterDefinition<AnalysisDocument> filter = Builders<AnalysisDocument>.Filter.Eq("_id", guid);
+                analysis = mongoAnalysisCollection.Find<AnalysisDocument>(filter).First<AnalysisDocument>();
+            }
+            catch (Exception e)
+            {
+                log.LogError("Error retrieving analysis for data: " + e.Message);
+                return new BadRequestObjectResult("Error retrieving configuration for data: " + e.Message);
+            }
+
             // Now you must verify the inputted object data against the configuration
-            VerifyData(config, document, log);
+            RobustnessAnalysis(config, document, log);
+
+            // Insert the analysis into the MongoDB analysis collection
+            log.LogInformation("Inserting analysis into MongoDB");
+            try
+            {
+                mongoAnalysisCollection.InsertOne(analysis);
+            }
+            catch (Exception e)
+            {
+                log.LogError("Failed inserting analysis document into MongoDB database: " + e.Message);
+                return new InternalServerErrorResult();
+            }
 
             // Insert the received object into the MongoDB object collection
             log.LogInformation("Inserting document into MongoDB");
@@ -136,9 +164,9 @@ namespace data_storage
          * - missing fields
          * - extra fields
          */
-        public static void VerifyData(BsonDocument config, BsonDocument document, ILogger log)
+        public static void RobustnessAnalysis(BsonDocument config, BsonDocument document, ILogger log)
         {
-            log.LogInformation("Beginning verification of data");
+            log.LogInformation("Beginning robustness analysis of data");
 
             int badValueCount = 0;
             int missingFieldsCount = 0;
@@ -164,8 +192,8 @@ namespace data_storage
                     // Since we have determined that the field is present, we can now check if the type matches
                     if (fieldType == "object")
                     {
-                        // Call VerifyData recursively on the nested object
-                        //VerifyData(fieldAttribute.GetValue("field_attributes"), fieldValue.ToBsonDocument(), log);
+                        // Call RobustnessAnalysis recursively on the nested object
+                        //RobustnessAnalysis(fieldAttribute.GetValue("field_attributes"), fieldValue.ToBsonDocument(), log);
                     }
                     if (fieldType != type)
                     {
@@ -186,6 +214,55 @@ namespace data_storage
             //int expectedNumberOfFields = config.field_attributes.ToList().Count;
 
 
+        }
+
+    }
+
+    /**
+      * Class used to represent an Analysis of each record type
+      * There should be one instance of this in the Analysis database for each config
+      */
+    public class AnalysisDocument
+    {
+
+        [BsonId]
+        public string ID { get; set; }
+
+        // the number of records for this ID
+        [BsonElement("NumberOfRecords")]
+        public int NumberOfRecords { get; set; }
+
+        // invalid values for the type of field, including null
+        [BsonElement("BadValueCount")]
+        public int BadValueCount { get; set; }
+
+        // data records with missing fields
+        [BsonElement("MissingFieldCount")]
+        public int MissingFieldCount { get; set; }
+
+        // data records with unexpected fields
+        [BsonElement("ExtraFieldCount")]
+        public int ExtraFieldCount { get; set; }
+
+        // keep track of one-time total failures
+        [BsonElement("OneTimeCommunicationFailureCount")]
+        public int OneTimeCommunicationFailureCount { get; set; }
+
+        // keep track of one-time total failures
+        [BsonElement("IntermittentCommunicationFailureCount")]
+        public int IntermittentCommunicationFailureCount { get; set; }
+
+        // array containing statistical analysis of all relevant data within the document
+        public BsonArray StatisticalAnalysis { get; set; }
+
+        public AnalysisDocument(string id)
+        {
+            this.ID = id;
+            this.BadValueCount = 0;
+            this.MissingFieldCount = 0;
+            this.ExtraFieldCount = 0;
+            this.OneTimeCommunicationFailureCount = 0;
+            this.IntermittentCommunicationFailureCount = 0;
         }
 
     }
