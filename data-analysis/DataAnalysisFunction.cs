06f953718be55947dbe77f8709af4053699f014d
Liam Bonnage <lbonnage@gmail.com>
2020-01-26 15:53:11 -0600

    AnalysisDocument class is now created within ConfigurationFunction and placed into analysis databasecollection DataStorage performs pre-processing on objects DataAnalysisFunction has infrastructure in place to perform post-processing but as of now only returns an unchanged AnalysisDocument

--- data-analysis/DataAnalysisFunction.cs
+++ data-analysis/DataAnalysisFunction.cs
@@ -7,27 +7,187 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Web.Http;
 
 namespace data_analysis
 {
+
     public static class DataAnalysisFunction
     {
+
+        // Static MongoDB handles for maintaining a persistent connection across service calls
+        static MongoClient mongoClient = null;
+        static IMongoDatabase mongoConsumerDatabase = null;
+        static IMongoCollection<BsonDocument> mongoConfigurationCollection = null;
+        static IMongoCollection<AnalysisDocument> mongoAnalysisCollection = null;
+        static Dictionary<string, IMongoCollection<BsonDocument>> mongoObjectCollections = new Dictionary<string, IMongoCollection<BsonDocument>>();
+
+        // Static list of numerical data types for performing numerical analysis on
+        static readonly List<string> numericalTypes = new List<string> { "int", "short", "ushort", "long", "uint", "ulong", "float", "double", "decimal" };
+
+
         [FunctionName("DataAnalysis")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Data analysis function received a request");
 
-            string name = req.Query["name"];
+            // Retrieve the GUID generated for this specific configuration
+            string guid = req.Headers["Config-GUID"];
+            if (guid is null)
+            {
+                log.LogError("Error retrieving Config-GUID header");
+                return new BadRequestObjectResult("Error retrieving Config-GUID header");
+            }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // In order to preserve the MongoDB client connection across Service calls, perform this check and only connect if necessary
+            if (mongoClient is null)
+            {
+                log.LogInformation("Connecting to MongoDB Database...");
+                try
+                {
+                    mongoClient = new MongoClient(System.Environment.GetEnvironmentVariable("MongoDBAtlasConnectionString"));
+                    mongoConsumerDatabase = mongoClient.GetDatabase(System.Environment.GetEnvironmentVariable("DeploymentEnvironment"));
+                    mongoConfigurationCollection = mongoConsumerDatabase.GetCollection<BsonDocument>("configurations");
+                    mongoAnalysisCollection = mongoConsumerDatabase.GetCollection<AnalysisDocument>("analysis");
+                    log.LogInformation("Connected to MongoDB Database");
+                }
+                catch (Exception e)
+                {
+                    log.LogError("Error connecting to MongoDB database: " + e.Message);
+                    return new InternalServerErrorResult();
+                }
+            }
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            // Make sure you have a handle to the collection for this kind of object
+            if (!mongoObjectCollections.ContainsKey(guid)) {
+                try
+                {
+                    mongoObjectCollections[guid] = mongoConsumerDatabase.GetCollection<BsonDocument>(guid);
+                }
+                catch (Exception e)
+                {
+                    log.LogError("Error retrieving object collection from MongoDB database: " + e.Message);
+                    return new InternalServerErrorResult();
+                }
+            }
+
+
+            // Retrieve the correct configuration document from the MongoDB database
+            log.LogInformation("Attempting to retrieve configuration for ID: " + guid);
+            BsonDocument config;
+            try
+            {
+                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", guid);
+                config = mongoConfigurationCollection.Find<BsonDocument>(filter).First<BsonDocument>();
+            }
+            catch (Exception e)
+            {
+                log.LogError("Error retrieving configuration for data: " + e.Message);
+                return new BadRequestObjectResult("Error retrieving configuration for data: " + e.Message);
+            }
+
+            // Retrieve the current analysis for this ID from the database
+            log.LogInformation("Attempting to retrieve analysis for ID: " + guid);
+            AnalysisDocument analysis;
+            try
+            {
+                FilterDefinition<AnalysisDocument> filter = Builders<AnalysisDocument>.Filter.Eq("_id", guid);
+                analysis = mongoAnalysisCollection.Find<AnalysisDocument>(filter).First<AnalysisDocument>();
+            }
+            catch (Exception e)
+            {
+                log.LogError("Error retrieving analysis for data: " + e.Message);
+                return new BadRequestObjectResult("Error retrieving configuration for data: " + e.Message);
+            }
+
+            // Retrieve a cursor to the objects in the appropriate collection
+            log.LogInformation("Attempting to retrieve cursor for collection: " + guid);
+            IAsyncCursor<BsonDocument> cursor;
+            try
+            {
+                cursor = mongoObjectCollections[guid].Find<BsonDocument>(f => true).ToCursor();
+            } catch (Exception e)
+            {
+                log.LogError("Error retrieving collection for data: " + e.Message);
+                return new BadRequestObjectResult("Error retrieving collection for data: " + e.Message);
+            }
+
+            // Iterate over all the documents in the collection and perform the analysis step on them
+            StatisticalAnalysis(ref analysis, config, cursor, log);
+
+            // Return the completed analysis to the requestor
+            return new OkObjectResult(analysis.ToJson());
         }
+
+        /**
+         * Performs statistical analysis on all the documents for a specific set of objects
+         * Should be able to find the following for all appropriate elements:
+         *  - Mean (numerical values)
+         *  - Standard deviation (numerical values)
+         */
+        public static void StatisticalAnalysis(ref AnalysisDocument analysis, BsonDocument config, IAsyncCursor<BsonDocument> cursor, ILogger log)
+        {
+
+            
+
+        }
+
     }
+
+    /**
+      * Class used to represent an Analysis of each record type
+      * There should be one instance of this in the Analysis database for each config
+      */
+    public class AnalysisDocument
+    {
+
+        [BsonId]
+        public string ID { get; set; }
+
+        // the number of records for this ID
+        [BsonElement("NumberOfRecords")]
+        public int NumberOfRecords { get; set; }
+
+        // invalid values for the type of field, including null
+        [BsonElement("BadValueCount")]
+        public int BadValueCount { get; set; }
+
+        // data records with missing fields
+        [BsonElement("MissingFieldCount")]
+        public int MissingFieldCount { get; set; }
+
+        // data records with unexpected fields
+        [BsonElement("ExtraFieldCount")]
+        public int ExtraFieldCount { get; set; }
+
+        // keep track of one-time total failures
+        [BsonElement("OneTimeCommunicationFailureCount")]
+        public int OneTimeCommunicationFailureCount { get; set; }
+
+        // keep track of one-time total failures
+        [BsonElement("IntermittentCommunicationFailureCount")]
+        public int IntermittentCommunicationFailureCount { get; set; }
+
+        // array containing statistical analysis of all relevant data within the document
+        public BsonArray StatisticalAnalysis { get; set; }
+
+        public AnalysisDocument(string id)
+        {
+            this.ID = id;
+            this.BadValueCount = 0;
+            this.MissingFieldCount = 0;
+            this.ExtraFieldCount = 0;
+            this.OneTimeCommunicationFailureCount = 0;
+            this.IntermittentCommunicationFailureCount = 0;
+        }
+
+    }
+
+
 }
